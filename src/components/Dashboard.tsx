b6b7af4b4309e32a6936b7a4a3b1b982ae768600
naumanijazch <24100311@lums.edu.pk>
2024-07-23 16:57:39 +0500

    add dynamic layout for homepage main component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"products"`, 
+        lg: `"products reports" "products summary"`,
+      }}
+      templateColumns={{
+        base: "1fr", 
+        lg: "2.7fr 1fr",
+      }}
+    >
+      <GridItem area="products" bg={"brown"}>
+        Products
+      </GridItem>
+      <GridItem
+        display={{ base: "none", lg: "block" }}
+        area="reports"
+        bg={"pink"}
+      >
+        reports
+      </GridItem>
+      <GridItem
+        display={{ base: "none", lg: "block" }}
+        area="summary"
+        bg={"purple"}
+      >
+        Summary
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default Dashboard;
