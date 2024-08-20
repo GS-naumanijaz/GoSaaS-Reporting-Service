2be5e0d278ecfc9af636c60f69770857e9dd6622
Safwan Siddique <safwansiddique01@gmail.com>
2024-08-20 12:46:43 +0500

    fixed issue with database reinitializing causing error on server restart

--- API/src/main/java/com/GRS/backend/utilities/DatabaseInitializer.java
+++ API/src/main/java/com/GRS/backend/utilities/DatabaseInitializer.java
@@ -1,5 +1,6 @@
 package com.GRS.backend.utilities;
 
+import jakarta.persistence.Query;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import jakarta.persistence.EntityManager;
@@ -16,38 +17,54 @@ public class DatabaseInitializer {
     @EventListener(ContextRefreshedEvent.class)
     @Transactional
     public void onApplicationEvent() {
-//        createUniqueIndexes();
+        createUniqueIndexes();
     }
 
     private void createUniqueIndexes() {
         try {
             // Applications
-            String applicationsSql = "CREATE UNIQUE INDEX unique_application_alias " +
-                    "ON applications (alias) " +
-                    "WHERE is_deleted = false;";
-            entityManager.createNativeQuery(applicationsSql).executeUpdate();
+            if (!isIndexExists("unique_application_alias")) {
+                String applicationsSql = "CREATE UNIQUE INDEX unique_application_alias " +
+                        "ON applications (alias) " +
+                        "WHERE is_deleted = false;";
+                entityManager.createNativeQuery(applicationsSql).executeUpdate();
+            }
 
             // Destination Connections
-            String destinationSql = "CREATE UNIQUE INDEX unique_destination_alias_per_application " +
-                    "ON destination_connections (app_id, alias) " +
-                    "WHERE is_deleted = false;";
-            entityManager.createNativeQuery(destinationSql).executeUpdate();
+            if (!isIndexExists("unique_destination_alias_per_application")) {
+                String destinationSql = "CREATE UNIQUE INDEX unique_destination_alias_per_application " +
+                        "ON destination_connections (app_id, alias) " +
+                        "WHERE is_deleted = false;";
+                entityManager.createNativeQuery(destinationSql).executeUpdate();
+            }
 
             // Source Connections
-            String sourceSql = "CREATE UNIQUE INDEX unique_source_alias_per_application " +
-                    "ON source_connections (app_id, alias) " +
-                    "WHERE is_deleted = false;";
-            entityManager.createNativeQuery(sourceSql).executeUpdate();
+            if (!isIndexExists("unique_source_alias_per_application")) {
+                String sourceSql = "CREATE UNIQUE INDEX unique_source_alias_per_application " +
+                        "ON source_connections (app_id, alias) " +
+                        "WHERE is_deleted = false;";
+                entityManager.createNativeQuery(sourceSql).executeUpdate();
+            }
 
             // Reports
-            String reportsSql = "CREATE UNIQUE INDEX unique_report_alias_per_application " +
-                    "ON reports (app_id, alias) " +
-                    "WHERE is_deleted = false;";
-            entityManager.createNativeQuery(reportsSql).executeUpdate();
+            if (!isIndexExists("unique_report_alias_per_application")) {
+                String reportsSql = "CREATE UNIQUE INDEX unique_report_alias_per_application " +
+                        "ON reports (app_id, alias) " +
+                        "WHERE is_deleted = false;";
+                entityManager.createNativeQuery(reportsSql).executeUpdate();
+            }
 
         } catch (Exception e) {
             // Log the exception or handle it appropriately
             System.err.println("Error creating unique indexes: " + e.getMessage());
         }
+    }
+
+    private boolean isIndexExists(String indexName) {
+        String sql = "SELECT COUNT(*) FROM pg_indexes WHERE indexname = :indexName";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("indexName", indexName);
+        Long count = ((Number) query.getSingleResult()).longValue();
+        return count > 0;
     }
 }
