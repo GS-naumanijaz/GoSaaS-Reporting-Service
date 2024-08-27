3683e7f746f25cbc6a8e6275229890f54c0a03f4
naumanijazch <24100311@lums.edu.pk>
2024-08-27 10:47:32 +0500

    fix bug in page redirection with sidebar

--- Front End/src/pages/Application.tsx
+++ Front End/src/pages/Application.tsx
@@ -1,27 +1,48 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { secondaryColor, tertiaryColor } from "../configs";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../components/Common/NavBar";
 import Sidebar from "../components/Common/Sidebar";
 import { useNavigate } from "react-router-dom";
 import AppDashboard from "../components/ApplicationPage/AppDashboard";
+import Requests from "../components/Requests/Requests";
+import AuditLogData from "../components/Data/AuditLogData";
 
 const Application = () => {
   const navigate = useNavigate();
-
   const [selectedComponent, setSelectedComponent] =
     useState<string>("Application");
+
+  useEffect(() => {
+    if (selectedComponent === "Dashboard") {
+      navigate("/homepage");
+    }
+  }, [selectedComponent, navigate]);
 
   const onSelected = (passedSelection: string) => {
     setSelectedComponent(passedSelection);
   };
+
+  const renderComponent = () => {
+    switch (selectedComponent) {
+      case "Application":
+        return <AppDashboard />;
+      case "AuditTrail":
+        return <AuditLogData />;
+      case "Dashboard":
+        navigate("/homepage");
+        return null;
+      default:
+        return <Requests />;
+    }
+  };
+
   return (
     <Grid
       templateAreas={`"nav nav" "sidebar main"`}
       templateColumns={{ base: "0fr", lg: "200px 1fr" }}
     >
-      <GridItem area="nav" bg={secondaryColor} >
+      <GridItem area="nav" bg={secondaryColor}>
         <NavBar />
       </GridItem>
       <Show above="lg">
@@ -30,11 +51,7 @@ const Application = () => {
         </GridItem>
       </Show>
       <GridItem area="main" bg={tertiaryColor}>
-        {selectedComponent === "Application" ? (
-          <AppDashboard />
-        ) : (
-          <>{navigate("/homepage")}</>
-        )}
+        {renderComponent()}
       </GridItem>
     </Grid>
   );
