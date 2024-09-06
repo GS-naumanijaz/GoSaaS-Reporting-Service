72fed6a048f770794dce6c5ebecacd5c68c56783
Safwan Siddique <safwansiddique01@gmail.com>
2024-09-06 17:09:37 +0500

    docker 1

--- API/src/main/java/com/GRS/backend/EnvConfig/PropertyConfig.java
+++ API/src/main/java/com/GRS/backend/EnvConfig/PropertyConfig.java
@@ -1,21 +1,21 @@
-package com.GRS.backend.EnvConfig;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PropertyConfig {
-
-    @Autowired
-    private Dotenv dotenv;
-
-    @PostConstruct
-    public void init() {
-        System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
-        System.setProperty("CLIENT_SECRET", dotenv.get("CLIENT_SECRET"));
-        System.setProperty("LOCAL_FRONTEND_URL", dotenv.get("LOCAL_FRONTEND_URL"));
-        System.setProperty("HOST_FRONTEND_URL", dotenv.get("HOST_FRONTEND_URL"));
-    }
-}
+//package com.GRS.backend.EnvConfig;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class PropertyConfig {
+//
+//    @Autowired
+//    private Dotenv dotenv;
+//
+//    @PostConstruct
+//    public void init() {
+//        System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
+//        System.setProperty("CLIENT_SECRET", dotenv.get("CLIENT_SECRET"));
+//        System.setProperty("LOCAL_FRONTEND_URL", dotenv.get("LOCAL_FRONTEND_URL"));
+//        System.setProperty("HOST_FRONTEND_URL", dotenv.get("HOST_FRONTEND_URL"));
+//    }
+//}
